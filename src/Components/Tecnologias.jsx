d1e7f4dfd887d5bea95968f4d0ba32ecae84971a
ivansandiyu <isandiyu@gmail.com>
2024-04-16 11:21:01 -0300

    actualizacion y correcion de errores

--- src/Components/Tecnologias.jsx
+++ src/Components/Tecnologias.jsx
@@ -5,19 +5,22 @@ import logoJavaScript from "../imagenes/JavaScript.svg";
 import logoSQL from "../imagenes/Postgresql.svg";
 import logoReact from "../imagenes/React.svg";
 
+
+
+
 function Tecnologias() {
+  
   return (
     <>
       <div className="cuadrado">
         <h1 className="texto">TECNOLOGIAS</h1>
         <div className="fila">
-          <img src={logoCsharp} alt="C#" id="csharp" />
-          <img src={logoJava} alt="Java" id = "java"/>
-          <img src={logoJavaScript} alt="JS" id="js"/>
-          <img src={logoReact} alt="React" id="react"/>
-          <img src={logoSQL} alt="PostgreSQL" id="sql"/>
+          <img src={logoCsharp} alt="C#" id="csharp" className="item"/>
+          <img src={logoJava} alt="Java" id = "java" className="item"/>
+          <img src={logoJavaScript} alt="JS" id="js" className="item"/>
+          <img src={logoReact} alt="React" id="react" className="item"/>
+          <img src={logoSQL} alt="PostgreSQL" id="sql" className="item"/>
         </div>
-       
       </div>
     </>
   );
