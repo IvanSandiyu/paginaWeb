8539507589a35c64ae415079d7b0bf153d2305cd
Ivan <isandiyu@gmail.com>
2024-04-23 12:17:20 -0300

    actualizacion pagina

--- src/Components/Proyectos.jsx
+++ src/Components/Proyectos.jsx
@@ -2,6 +2,7 @@ import "../css/Proyectos.css";
 import crud from "../imagenes/crud.png";
 import java from "../imagenes/java.jpg";
 import bannerC from "../imagenes/bannerC.jpg";
+import asp from "../imagenes/asp.png";
 
 const AbrirGit = () => {
   const enlaceTemporal = document.createElement("a");
@@ -22,6 +23,13 @@ const AbrirGitC = () => {
   const enlaceTemporal = document.createElement("a");
   // Asignar la URL del archivo a descargar al atributo 'href' del enlace
   enlaceTemporal.href = "https://github.com/IvanSandiyu/Alta.Baja.Mesa";
+  enlaceTemporal.target = "_blank";
+  enlaceTemporal.click();
+};
+const AbrirGitASP = () => {
+  const enlaceTemporal = document.createElement("a");
+  // Asignar la URL del archivo a descargar al atributo 'href' del enlace
+  enlaceTemporal.href = "https://github.com/IvanSandiyu/CRUD-ASP.NET";
   enlaceTemporal.target = "_blank";
   enlaceTemporal.click();
 };
@@ -49,6 +57,10 @@ function Proyectos() {
         <div className="bannerCsharp">
           <p className="texto-c">Proyectos realizados con C#</p>
           <img src={bannerC} alt="c#" id="c-proyecto" onClick={AbrirGitC} />
+        </div>
+        <div className="bannerASP">
+          <p className="texto-asp">Proyecto realizado con ASP.NET</p>
+          <img src={asp} alt="ASP" id="asp-proyecto" onClick={AbrirGitASP} />
         </div>
       </div>
     </>
