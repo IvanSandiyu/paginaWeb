a93c4d12a830554414cd60fd412193ee5eb1a5d3
Ivan <isandiyu@gmail.com>
2024-05-08 11:27:54 -0300

    le agregue la pag nueva

--- src/Components/Actualizacion.jsx
+++ src/Components/Actualizacion.jsx
@@ -1,8 +1,22 @@
+
+const nuevaPag = () => {
+  const enlaceTemporal = document.createElement('a');
+  // Asignar la URL del archivo a descargar al atributo 'href' del enlace
+  enlaceTemporal.href = "https://new-pagina.vercel.app/"
+  enlaceTemporal.target = "_blank"
+  enlaceTemporal.click();
+};
+
+const estilosH1 = {
+  cursor: 'pointer' ,
+};
+
 function Actualizacion() {
   return (
     <>
+    
       <div className="actualizacion">
-        <h1>Actualizando pagina y proyectos..Muchas gracias por ver</h1>
+        <h1 onClick={nuevaPag} style={estilosH1}>Nueva pagina en proceso haga click aquí</h1>
       </div>
     </>
   );
